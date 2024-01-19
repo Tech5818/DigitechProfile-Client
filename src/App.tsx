d15f507c618ef1cc60ba0e9bf6af1bcedef1dcf0
Tech5818 <pqow5818@gmail.com>
2024-01-19 18:07:52 +0900

    :sparkles: HomePage loadable-component라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import loadable from "@loadable/component";
+import { Route, Routes } from "react-router-dom";
+
+const HomePage = loadable(() => import("./page/HomePage"));
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} loader={HomePage.load} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
