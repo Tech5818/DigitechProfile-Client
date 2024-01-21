e9faf25f7da4383853785dae952ced40bf955550
Tech5818 <pqow5818@gmail.com>
2024-01-21 22:18:52 +0900

    :sparkles: Header Component 모바일 반응형 설정 및 css 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import loadable from "@loadable/component";
 import { Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
 
 const HomePage = loadable(() => import("./page/HomePage"));
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} loader={HomePage.load} />
       </Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 export const Header = () => {
   return (
     <>
-      <Box />
+      <Box boxShadow="0 4px 4px rgba(0, 0, 0, 0.2)" height="80px"></Box>
     </>
   );
 };
