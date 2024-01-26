387608c48ae130858425b9adf660937791f7af05
Tech5818 <pqow5818@gmail.com>
2024-01-26 22:13:06 +0900

    :sparkles: react-query Provider 설정

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,13 +1,18 @@
 import { PropsWithChildren } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export const Provider = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <RecoilRoot>
-        <BrowserRouter>{children}</BrowserRouter>
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <BrowserRouter>{children}</BrowserRouter>
+        </RecoilRoot>
+      </QueryClientProvider>
     </>
   );
 };
